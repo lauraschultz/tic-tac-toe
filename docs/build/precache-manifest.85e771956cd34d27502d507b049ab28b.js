96171f98104ae4665b2096318ea1b1c553f2a943
lauraschultz <schultzlaurac@gmail.com>
2020-06-09 14:33:29 -0500

    change base href

---
+++ docs/build/precache-manifest.85e771956cd34d27502d507b049ab28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a297bbd3bab1d0e9125c3324ba1288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b50bd4e31827e6b797bd",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "788bf36ecea4c4a333c0",
+    "url": "/static/js/2.dc270b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc270b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50bd4e31827e6b797bd",
+    "url": "/static/js/main.863191d9.chunk.js"
+  },
+  {
+    "revision": "6f92bc462155ad9de6a6",
+    "url": "/static/js/runtime-main.550eb164.js"
+  }
+]);
